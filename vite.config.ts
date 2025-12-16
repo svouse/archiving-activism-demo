2d60a73c32a59be56a1021898d8beaa6d97d2953
Sage Vousé <svouse@gmail.com>
2025-12-16 01:01:54 -0500

    update app

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         index: resolve(__dirname, 'index.html'),
+        archives: resolve(__dirname, 'public/pages/archives.html'),
         search: resolve(__dirname, 'public/pages/search.html'),
       },
     },
